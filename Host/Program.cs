525c304a6cf95702d29ff27c30e93e0faf441c62
Manan <54689328+mananab11@users.noreply.github.com>
2020-05-25 13:10:10 -0400

    Add files via upload

---
+++ Host/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MessageLibrary;
+using System.ServiceModel.Description;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Broadcast) );
+            
+            try
+            {
+                host.Open();
+                DisplayEndpoints(host);
+                Console.ReadLine();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                host.Abort();
+            }
+        }
+
+        private static void DisplayEndpoints(ServiceHost host)
+        {
+            foreach(ServiceEndpoint collection in host.Description.Endpoints)
+            {
+                Console.WriteLine("running {0} at  {1} ",collection.Name,collection.Address);
+
+            }
+        }
+    }
+}
